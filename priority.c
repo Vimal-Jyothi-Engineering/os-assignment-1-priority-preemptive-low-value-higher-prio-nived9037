00284e5602e615cb23a7fc4e00cba691e42c715f
nived9037 <nived9037@gmail.com>
2026-03-10 22:33:56 +0530

    Assignment - 2 completed

--- priority.c
+++ priority.c
@@ -1,71 +1,85 @@
 #include <stdio.h>
 
-struct process{
-    char pid[10];
-    int at, bt, pr;
-    int rt;
-    int ct, tat, wt;
-};
+#define MAX 10
 
-int main()
-{
-    int n,time=0,completed=0;
-    float avg_wt=0,avg_tat=0;
+typedef struct {
+    int pid, at, bt, pri, rt, ct, tat, wt;
+} Proc;
 
-    struct process p[20];
+int main() {
+    int n;
+    scanf("%d", &n);
 
-    scanf("%d",&n);
-
-    for(int i=0;i<n;i++)
-    {
-        scanf("%s %d %d %d",p[i].pid,&p[i].at,&p[i].bt,&p[i].pr);
-        p[i].rt=p[i].bt;
+    Proc p[MAX];
+    for (int i = 0; i < n; i++) {
+        char label[16];
+        scanf("%s %d %d %d", label, &p[i].at, &p[i].bt, &p[i].pri);
+        p[i].pid = 0;
+        for (int k = 0; label[k]; k++)
+            if (label[k] >= '0' && label[k] <= '9')
+                p[i].pid = p[i].pid * 10 + (label[k] - '0');
+        p[i].rt = p[i].bt;
+        p[i].ct = p[i].tat = p[i].wt = 0;
     }
 
-    while(completed<n)
-    {
-        int idx=-1;
+    for (int i = 1; i < n; i++) {
+        Proc key = p[i]; int j = i - 1;
+        while (j >= 0 && p[j].at > key.at) { p[j+1] = p[j]; j--; }
+        p[j+1] = key;
+    }
 
-        for(int i=0;i<n;i++)
-        {
-            if(p[i].at<=time && p[i].rt>0)
-            {
-                if(idx==-1 || p[i].pr < p[idx].pr)
-                    idx=i;
+    int done = 0, t = p[0].at;
+    for (int i = 1; i < n; i++) if (p[i].at < t) t = p[i].at;
+
+    while (done < n) {
+        int sel = -1;
+        for (int i = 0; i < n; i++) {
+            if (p[i].at <= t && p[i].rt > 0) {
+                if (sel < 0 || p[i].pri < p[sel].pri ||
+                    (p[i].pri == p[sel].pri && p[i].at < p[sel].at))
+                    sel = i;
             }
         }
 
-        if(idx==-1)
-        {
-            time++;
+        if (sel < 0) {
+            int nxt = -1;
+            for (int i = 0; i < n; i++)
+                if (p[i].rt > 0 && (nxt < 0 || p[i].at < nxt)) nxt = p[i].at;
+            t = nxt;
             continue;
         }
 
-        p[idx].rt--;
-        time++;
+        int nxt = t + p[sel].rt;
+        for (int i = 0; i < n; i++) {
+            if (i == sel || p[i].rt <= 0) continue;
+            if (p[i].at > t && p[i].at < nxt) nxt = p[i].at;
+        }
 
-        if(p[idx].rt==0)
-        {
-            completed++;
+        p[sel].rt -= (nxt - t);
+        t = nxt;
 
-            p[idx].ct=time;
-            p[idx].tat=p[idx].ct-p[idx].at;
-            p[idx].wt=p[idx].tat-p[idx].bt;
-
-            avg_wt+=p[idx].wt;
-            avg_tat+=p[idx].tat;
+        if (p[sel].rt == 0) {
+            p[sel].ct  = t;
+            p[sel].tat = t - p[sel].at;
+            p[sel].wt  = p[sel].tat - p[sel].bt;
+            done++;
         }
     }
 
-    for(int i=0;i<n;i++)
-    {
-        printf("%s %d %d %d %d %d %d\n",
-        p[i].pid,p[i].at,p[i].bt,p[i].pr,
-        p[i].ct,p[i].tat,p[i].wt);
-    }
+    float twt = 0, ttat = 0;
 
-    printf("Average Waiting Time: %.2f\n",avg_wt/n);
-    printf("Average Turnaround Time: %.2f\n",avg_tat/n);
+    printf("Waiting Time:\n");
+    for (int pid = 1; pid <= n; pid++)
+        for (int i = 0; i < n; i++)
+            if (p[i].pid == pid) { printf("P%d %d\n", p[i].pid, p[i].wt); twt += p[i].wt; break; }
+
+    printf("Turnaround Time:\n");
+    for (int pid = 1; pid <= n; pid++)
+        for (int i = 0; i < n; i++)
+            if (p[i].pid == pid) { printf("P%d %d\n", p[i].pid, p[i].tat); ttat += p[i].tat; break; }
+
+    printf("Average Waiting Time: %.2f\n",    twt  / n);
+    printf("Average Turnaround Time: %.2f\n", ttat / n);
 
     return 0;
 }
