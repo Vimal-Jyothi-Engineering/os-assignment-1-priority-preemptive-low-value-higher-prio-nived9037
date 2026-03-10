de3cef1737cef7497b0fc0db6ca2872742f6eba4
nived9037 <nived9037@gmail.com>
2026-03-10 22:24:03 +0530

    Assignment - 2 completed

--- priority.c
+++ priority.c
@@ -1,69 +1,59 @@
 #include <stdio.h>
 
-struct process
-{
+struct process{
     char pid[10];
     int at, bt, pr;
-    int ct, wt, tat;
     int rem;
+    int ct, wt, tat;
 };
 
 int main()
 {
-    int n, time = 0, completed = 0, idx;
-    float avg_wt = 0, avg_tat = 0;
+    int n, time=0, completed=0, idx;
+    float avg_wt=0, avg_tat=0;
 
     struct process p[20];
 
     scanf("%d",&n);
 
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         scanf("%s %d %d %d",p[i].pid,&p[i].at,&p[i].bt,&p[i].pr);
         p[i].rem = p[i].bt;
     }
 
-    while(completed < n)
+    while(completed<n)
     {
         idx = -1;
 
         for(int i=0;i<n;i++)
         {
-            if(p[i].at <= time && p[i].rem > 0)
+            if(p[i].at<=time && p[i].rem>0)
             {
-                if(idx == -1 || p[i].pr < p[idx].pr)
-                    idx = i;
+                if(idx==-1 || p[i].pr < p[idx].pr)
+                    idx=i;
             }
         }
 
-        if(idx != -1)
+        if(idx!=-1)
         {
             p[idx].rem--;
             time++;
 
-            if(p[idx].rem == 0)
+            if(p[idx].rem==0)
             {
                 completed++;
+                p[idx].ct=time;
+                p[idx].tat=p[idx].ct-p[idx].at;
+                p[idx].wt=p[idx].tat-p[idx].bt;
 
-                p[idx].ct = time;
-                p[idx].tat = p[idx].ct - p[idx].at;
-                p[idx].wt = p[idx].tat - p[idx].bt;
-
-                avg_wt += p[idx].wt;
-                avg_tat += p[idx].tat;
+                avg_wt+=p[idx].wt;
+                avg_tat+=p[idx].tat;
             }
         }
         else
         {
             time++;
         }
-    }
-
-    for(int i=0;i<n;i++)
-    {
-        printf("%s %d %d %d %d %d %d\n",
-        p[i].pid,p[i].at,p[i].bt,p[i].pr,
-        p[i].ct,p[i].tat,p[i].wt);
     }
 
     printf("Average Waiting Time: %.2f\n",avg_wt/n);
